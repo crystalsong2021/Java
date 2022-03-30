00f6497d7c07256333e60314d8b098a10c5c9942
crystal <2022csong@gmail.com>
2022-03-29 20:42:21 -0700

    primitive

--- Hello.java
+++ Hello.java
@@ -1,11 +1,22 @@
 /**
  * Hello
  */
+import java.util.*;
 public class Hello {
 
   public static void main (String[] args) {
     int myAge = 30;
     int herAge = myAge;
+    long viewsCount = 3_223_333_334L;
+    float price = 10.09F;
+    char letter = 'A';
+    boolean isEligible = true;
+
+    Date now = new Date();
+    now.getTime();
+    System.out.println(now);
+    System.out.println(herAge );
     System.out.println(myAge);
   }
+
 }
