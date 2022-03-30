e4c39a060c2a907e81b14750731f66181d9ab95c
crystal <2022csong@gmail.com>
2022-03-29 20:25:56 -0700

    type int

--- Hello.java
+++ Hello.java
@@ -4,6 +4,8 @@
 public class Hello {
 
   public static void main (String[] args) {
-    System.out.println("-------------Hello!---------------");
+    int myAge = 30;
+    int herAge = myAge;
+    System.out.println(myAge);
   }
 }
