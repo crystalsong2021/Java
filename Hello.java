8422a863ebc9ea85ce1d45b028392777b493a97a
crystal <2022csong@gmail.com>
2022-03-30 13:04:28 -0700

    string method

--- Hello.java
+++ Hello.java
@@ -1,22 +1,31 @@
 /**
  * Hello
  */
-import java.util.*;
+
 public class Hello {
 
-  public static void main (String[] args) {
-    int myAge = 30;
-    int herAge = myAge;
-    long viewsCount = 3_223_333_334L;
-    float price = 10.09F;
-    char letter = 'A';
-    boolean isEligible = true;
 
-    Date now = new Date();
-    now.getTime();
-    System.out.println(now);
-    System.out.println(herAge );
-    System.out.println(myAge);
+  public static void main (String[] args) {
+    // int myAge = 30;
+    // int herAge = myAge;
+    // long viewsCount = 3_223_333_334L;
+    // float price = 10.09F;
+    // char letter = 'A';
+    // boolean isEligible = true;
+
+    // Date now = new Date();
+    // now.getTime();
+    // System.out.println(now);
+    // Point point1 = new Point( x : 1, y : 1); //reference variable
+    // Point point2 = point1;
+    // point1.x = 2;
+    //reference type are copy by the reference, primitive type are copy by the value
+    String message = "Hello World" + "!";
+    message.endsWith("!");
+    System.out.println(message.length() < 14);
+
+
+
   }
 
 }
